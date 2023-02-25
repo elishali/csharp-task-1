e1523d29d7abcdd7aa7363913d396b150df8f872
elishali <elnarama.code.edu.az>
2023-02-26 01:56:46 +0400

    Taski tamamladim

---
+++ ConsoleAppTask1/Program.cs
@@ -0,0 +1,215 @@
+﻿namespace ConsoleAppTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ci sual
+            /*Console.WriteLine("4 reqemli eded verilib.Bu ededin eveline 7 reqemini, axirinada 8 reqemini artir");
+
+             Console.WriteLine("4 reqemli ededi daxil edin");
+
+             int n =
+
+                 int.Parse(Console.ReadLine());
+
+             int a = n * 10 + 8 + 700000;
+
+             Console.WriteLine($" {a} ");*/
+
+
+            //2 ci sual
+            /*Console.WriteLine(" 3 reqemli eded verilib. Bu ededin axirina hemin ededdin ozunu yapishdir");
+
+             Console.WriteLine("3 reqemli eded daxil edin");
+
+             int n = 
+
+                 int.Parse(Console.ReadLine());
+
+             int a = n * 1000 + n;
+
+             Console.WriteLine($" {a} ");*/
+
+
+            //3 cu sual
+            /*Console.WriteLine(" 5 reqemli eded verilib. Bu ededin evvel  18 % sonra ise 3 % tap.");
+
+            Console.WriteLine("5 reqemli eded daxil edin");
+
+            double n =
+
+                int.Parse(Console.ReadLine());
+
+            double a = (n * 18) / 100;
+
+            double b = (n * 3) / 100;
+
+            Console.WriteLine($" ededin 18%-i {a}, ededin 3%-i {b} ");*/
+
+
+            //4 cu sual
+            /*Console.WriteLine(" 3 reqemli eded verilib. Bu ededin axirina 7 artir. Sonra cavabin 7% tap;");
+
+            Console.WriteLine("3 reqemli eded daxil edin");
+
+            double n =
+
+                double.Parse(Console.ReadLine());
+
+            double a = n * 10 + 7;
+
+            double b = (a * 7) / 100;
+
+            Console.WriteLine($"sonuna 7 elave edildikden sonra alinan eded= {a}, alinmis ededin 7%-i={b} ");*/
+
+
+            //5 ci sual
+            /*Console.WriteLine(" 4 reqemli eded verilib. Bu ededin evvelin 4 reqemi , axirina ise 44 ededini artir. Alinan cavabin 44 % tap.");
+
+             Console.WriteLine("4 reqemli eded daxil edin");
+
+             double n =
+
+                 double.Parse(Console.ReadLine());
+
+             double a = (n * 100) + 44 + 4000000;
+
+             double b = (a * 44) / 100;
+
+             Console.WriteLine($"eded = {a}, alinmis ededin 44%-i= {b}");*/
+
+
+            //6 ci sual
+            /*Console.WriteLine(" 4 reqemli eded verilib. Bu ededin evvel 20%-ni ,  sonra ise cavabin 10% tap.  Alinan  cavabin kvadratini tap. ");
+
+            Console.WriteLine("4 reqemli eded daxil edin");
+
+            double n =
+
+                double.Parse(Console.ReadLine());
+
+            double a = (n * 20) / 100;
+
+            double b = (a * 10) / 100;
+
+            double c = b * b;
+
+            Console.WriteLine($"dedin 20%-i={a}, alinmis ededin 10%-i={b}, alinan cavabin kvadrati ={c}");*/
+
+
+            //7 ci sual
+            /*Console.WriteLine("2 dene 5 reqemli eded verilib. Bu ededleri toplayib. Alinan cavabin evveline ve axirina 5 artir. Neticenin 5 % tap");
+
+           Console.WriteLine("1 ci(5 reqemli eded daxil edin):");
+
+           double n =
+
+               double.Parse(Console.ReadLine());
+
+           Console.WriteLine("2 ci(5 reqemli eded daxiledin):");
+
+           double n1=
+
+               double.Parse(Console.ReadLine());
+
+           double a = n + n1;
+
+           double b = (a * 10) + 5 + 5000000;
+
+           double c = (b * 5) / 100;
+
+           Console.WriteLine($"evveline ve sonuna 5 artirilan = {b}, alinan neticenin 5%-i = {c}");*/
+
+
+            //8 ci sual
+            /* Console.WriteLine(" 2 dene eded verilib. I eded 4 reqemli II eded 7 reqemlidir. I ededin 4%-ni tap. Sonra II ededin 9% ni tap. Sonra Cavalari toplayib 10 %ni tap. ");
+
+             Console.WriteLine("1-ci (4 reqemli eded daxil edin):");
+
+             double n =
+
+                 double.Parse(Console.ReadLine());
+
+             Console.WriteLine("2-ci (7 reqemli ededi daxil edin):");
+
+             double n1 =
+
+                 double.Parse(Console.ReadLine());
+
+             double a = (n * 4) / 100;
+
+             double b = (n1 * 9) / 100;
+
+             double c = ((a + b) * 10) / 100;
+
+             Console.WriteLine($"1 ci ededin 4%-i={a}, 2 ci ededin 9%-i={b}, ededlerin ceminin 10%-i={c}");*/
+
+
+            //9 cu sual
+            /*Console.WriteLine(" 3 dene 6 reqemli eded verilib. Her birinin 10 faizini tapib neticeleri topla. Alinan cavabin 10% tap.");
+
+            Console.WriteLine("1 ci (6 reqemli eded daxil edin):");
+
+            double n =
+
+                double.Parse(Console.ReadLine());
+
+            Console.WriteLine("2 ci (6 reqemli eded daxil edin):");
+
+            double n1 =
+
+                double.Parse(Console.ReadLine());
+
+            Console.WriteLine("3 cu (6 reqemli eded daxil edin):");
+
+            double n2 =
+
+                double.Parse(Console.ReadLine());
+
+            double a = (n * 10) / 100;
+
+            double b = (n1 * 10) / 100;
+
+            double c = (n2 * 10) / 100;
+
+            double d = ((a + b + c) * 10) / 100;
+
+            Console.WriteLine($"alinan neticelerin toplaminin 10%-i={d}");*/
+
+
+            //10 cu sual
+            /*Console.WriteLine("3 dene 4 reqemli eded verilib.  I ededin 1%-ni, II ededin 2% , III ededin 3 % tap. Neticeleri bir birinden cix. Alinan cavabin ustune III ededin 7% faizini gel");
+
+            Console.WriteLine("1 ci(4 reqemli eded daxil edin):");
+
+            double n =
+
+                double.Parse(Console.ReadLine());
+
+            Console.WriteLine("2 ci(4 reqemli eded daxil edin):");
+
+            double n1 =
+
+                double.Parse(Console.ReadLine());
+
+            Console.WriteLine("3 cu(4 reqemli eded daxil edin):");
+
+            double n2 =
+
+                double.Parse(Console.ReadLine());
+
+            double a = (n * 1) / 100;
+
+            double b = (n1 * 2) / 100;
+
+            double c = (n2 * 3) / 100;
+
+            double c1 = (n2 * 7) / 100;
+
+            double d = (a - b - c) + c1;
+
+            Console.WriteLine($"alinan cavabin neticesi = {d}");*/
+        }
+    }
+}
